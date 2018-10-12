8a5bf73af2995a9ce5494e9bae2af8c581b7b7e8
Stef Goor <r0663065@student.thomasmore.be>
2018-10-12 11:30:53 +0200

    Stef - Voorbeeld namaken voor transportmiddel

--- src/main/java/be/thomasmore/travelmore/controller/TransportmiddelController.java
+++ src/main/java/be/thomasmore/travelmore/controller/TransportmiddelController.java
@@ -1,4 +1,35 @@
 package be.thomasmore.travelmore.controller;
 
+import be.thomasmore.travelmore.domain.Transportmiddel;
+import be.thomasmore.travelmore.service.TransportmiddelService;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
 public class TransportmiddelController {
+    private Transportmiddel newTransportmiddel = new Transportmiddel();
+
+    @Inject
+    private TransportmiddelService transportmiddelService;
+
+    public Transportmiddel getNewTransportmiddel() {
+        return newTransportmiddel;
+    }
+
+    public void setNewTransportmiddel(Transportmiddel newTransportmiddel) {
+        this.newTransportmiddel = newTransportmiddel;
+    }
+
+    public List<Transportmiddel> getTransportmiddelen(){
+        return this.transportmiddelService.findAllTransportmiddelen();
+    }
+
+    public void submit(){
+        this.transportmiddelService.insert(newTransportmiddel);
+    }
+
 }
