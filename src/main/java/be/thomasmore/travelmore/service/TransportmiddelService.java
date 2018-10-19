30bf31af153e3c369970b41825cd084fc1a6cac3
ethanvdb <ethanvdb@hotmail.com>
2018-10-19 10:31:41 +0200

    persistence

--- src/main/java/be/thomasmore/travelmore/service/TransportmiddelService.java
+++ src/main/java/be/thomasmore/travelmore/service/TransportmiddelService.java
@@ -1,11 +1,11 @@
 package be.thomasmore.travelmore.service;
 
-import be.thomasmore.travelmore.domain.Transportmiddel;
-import be.thomasmore.travelmore.repository.TransportmiddelRepository;
+        import be.thomasmore.travelmore.domain.Transportmiddel;
+        import be.thomasmore.travelmore.repository.TransportmiddelRepository;
 
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import java.util.List;
+        import javax.ejb.Stateless;
+        import javax.inject.Inject;
+        import java.util.List;
 
 @Stateless
 public class TransportmiddelService {
