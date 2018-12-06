fa48894542e3ca3176372b8a70d8b87846150f4a
ethanvdb <ethanvdb@hotmail.com>
2018-12-06 16:57:44 +0100

    populaire bestemmingen sql query

--- src/main/java/be/thomasmore/travelmore/domain/Plaats.java
+++ src/main/java/be/thomasmore/travelmore/domain/Plaats.java
@@ -1,5 +1,6 @@
 package be.thomasmore.travelmore.domain;
 
+import javax.inject.Named;
 import javax.persistence.*;
 import javax.persistence.GenerationType;
 import be.thomasmore.travelmore.domain.Reis;
@@ -25,6 +26,12 @@ import java.util.Date;
                 @NamedQuery(
                         name= Plaats.FIND_ALL_VERTREKPLAATSEN,
                         query = "select p from Plaats p where p.id IN (SELECT r.vertrekPlaats.id from Reis r)"
+                ),
+                @NamedQuery(
+                        name= Plaats.FIND_POPULAIRE_BESTEMMINGEN,
+                        query = "select p from Plaats p where p.id in (SELECT r.bestemming.id from Reis r)" +
+                                " group by p.id order by (SELECT count(b) from Boeking b where b.reis.id in " +
+                                "(select r.id from Reis r where r.bestemming.id = p.id)) desc limit 3"
                 )
         }
 )
@@ -34,6 +41,7 @@ public class Plaats {
     public static final String FIND_BY_ID = "Plaats.findById";
     public static final String FIND_ALL_BESTEMMINGEN = "Plaats.findAllBestemmingen";
     public static final String FIND_ALL_VERTREKPLAATSEN = "Plaats.findAllVertrekplaatsen";
+    public static final String FIND_POPULAIRE_BESTEMMINGEN = "Plaats.findPopulaireBestemmingen";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
